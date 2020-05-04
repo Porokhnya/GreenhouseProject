907ac57e521fcd800b6a558de4ee11fed0541a58
Porokhnya Dmitry <spywarrior@gmail.com>
2020-05-04 10:25:53 +0300

    pre-alpha, WI-FI execution module

--- UniversalWiFiExecutionModule/UniversalWiFiExecutionModule.ino
+++ UniversalWiFiExecutionModule/UniversalWiFiExecutionModule.ino
@@ -67,6 +67,163 @@
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 bool wifiConnected = false;
 WiFiUDP Udp;
+SlotSettings slots[] = { SLOTS };
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+void setupSlots()
+{
+  size_t cnt = sizeof(slots)/sizeof(SlotSettings);
+  
+  for(size_t i=0;i<cnt;i++)
+  {
+    if(slots[i].type == NO_BINDING) // нет привязки
+    {
+      continue;
+    }
+
+    pinMode(slots[i].pin,OUTPUT);
+    digitalWrite(slots[i].pin, !(POWER_ON_LEVEL));
+  }
+}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+uint8_t MakeNum(char ch) 
+{
+  if((ch >= '0') && (ch <= '9'))
+    return ((uint8_t) ch) - '0';
+  
+  switch(ch) 
+  {
+    case 'A':
+    case 'a': return 10;
+    
+    case 'B':
+    case 'b': return 11;
+    
+    case 'C':
+    case 'c': return 12;
+    
+    case 'D':
+    case 'd': return 13;
+
+    case 'E':
+    case 'e': return 14;
+    
+    case 'F':
+    case 'f': return 15;
+    
+    default: return 16;
+    }
+}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+uint8_t FromHex(const char* buff)
+{
+  uint8_t tens = MakeNum(*buff++);
+  uint8_t ones = MakeNum(*buff);
+
+   if(ones == 'X') 
+    return  0;
+    
+  return  (tens * 16) + ones; 
+}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+void dealWithPacket(const char* packet)
+{
+  // разбираем пакет, превращая его в структуру ControllerState
+  
+  ControllerState state;
+  uint8_t* pState = (uint8_t*)&state;
+
+  // поскольку у нас строка вида F0DEFE18..., то нам надо каждый байт раскодировать из двух символов, и записать последовательно в структуру
+  
+  for(size_t i=0;i<sizeof(ControllerState);i++)
+  {
+    uint8_t bVal = FromHex(packet);
+    *pState = bVal;
+    pState++;
+    packet++; if(!*packet) { break; }  // перемещаемся на следующий байт
+    packet++; if(!*packet) { break; }  // перемещаемся на следующий байт
+  }
+
+  // структура сформирована, можем с ней работать
+  size_t cnt = sizeof(slots)/sizeof(SlotSettings);
+
+  // пробегаем по всем слотам
+  for(size_t i=0;i<cnt;i++)
+  {
+    if(slots[i].type == NO_BINDING) // нет привязки
+    {
+      continue;
+    }
+
+      // смотрим тип привязки
+      switch(slots[i].type)
+      {
+        case WINDOW:  // канал окна
+        {
+            uint8_t bit_num = slots[i].link*2;
+            if(slots[i].param == RIGHT_CHANNEL)
+            {
+              bit_num++;
+            }
+
+            if(state.WindowsState & (1 << bit_num)) // канал включен
+            {
+              digitalWrite(slots[i].pin, (POWER_ON_LEVEL));
+            }
+            else // канал выключен
+            {
+              digitalWrite(slots[i].pin, !(POWER_ON_LEVEL));
+            }            
+        }
+        break;
+
+        case WATER:   // канал полива
+        {
+            if(state.WaterChannelsState & (1 << slots[i].link)) // канал включен
+            {
+              digitalWrite(slots[i].pin, (POWER_ON_LEVEL));
+            }
+            else // канал выключен
+            {
+              digitalWrite(slots[i].pin, !(POWER_ON_LEVEL));
+            }          
+        }
+        break;
+
+        case LIGHT:   // канал досветки
+        {
+            if(state.LightChannelsState & (1 << slots[i].link)) // канал включен
+            {
+              digitalWrite(slots[i].pin, (POWER_ON_LEVEL));
+            }
+            else // канал выключен
+            {
+              digitalWrite(slots[i].pin, !(POWER_ON_LEVEL));
+            }
+        }
+        break;
+
+        case VPIN:  // виртуальный пин
+        {
+            uint8_t byte_num = slots[i].link/8;
+            uint8_t bit_num = slots[i].link%8;
+
+            uint8_t bVal = state.PinsState[byte_num];
+            
+            if(bVal & (1 << bit_num)) // пин включен
+            {
+              digitalWrite(slots[i].pin, (POWER_ON_LEVEL));
+            }
+            else // пин выключен
+            {
+              digitalWrite(slots[i].pin, !(POWER_ON_LEVEL));
+            }
+        }
+        break;
+        
+      } // switch
+
+  } // for
+}
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 void myDelay(uint32_t msec)
 {
@@ -202,7 +359,7 @@ void parseIncomingUDPPackets()
       Serial.println(packet);
     #endif
 
-    //TODO: ТУТ РАЗБОР ПАКЕТА !!!
+    dealWithPacket(packet);
 
     delete [] packet;
   }
@@ -222,6 +379,8 @@ void setup()
   WiFi.setAutoReconnect(false);
 
   Settings.begin();
+
+  setupSlots();
 
   #ifdef _DEBUG
     Serial.println(F("Started."));
